f6ee07805e2cc91a3d5102e5fcae781d82a17d44
Knifa <knifa@users.noreply.github.com>
2024-11-27 13:46:11 +0000

    move back to led_matrix_zmq domain in prep. for adding to brands

--- custom_components/led_matrix_zmq/const.py
+++ custom_components/led_matrix_zmq/const.py
@@ -7,7 +7,7 @@ from homeassistant.const import Platform
 
 LOGGER: Final = logging.getLogger(__package__)
 
-DOMAIN: Final = "lmz"
+DOMAIN: Final = "led_matrix_zmq"
 PLATFORMS: Final = [Platform.LIGHT]
 
 CONF_NAME: Final = "name"
